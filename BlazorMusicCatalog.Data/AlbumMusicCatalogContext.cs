74b4aafa17ea59935a2e679d914300e47a7847a2
Javier <jvalverde@prextechnologies.com>
2022-10-17 17:27:13 -0500

    Add project files.

---
+++ BlazorMusicCatalog.Data/AlbumMusicCatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using BlazorMusicCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMusicCatalog.Data
+{
+    public  class AlbumMusicCatalogContext: DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;User ID=sa;Password=Metallica2011;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=AlbumMusicCatalog");
+        }
+    }
+}
